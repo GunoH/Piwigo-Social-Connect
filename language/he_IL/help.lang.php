cbc51d56f0d84551fa81f91e43d9f43d06d89e25
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 16:19:36 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/help.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Based on OpenID'] = 'מבוסס על OpenID';
+$lang['Click on <b>Add Platform</b>, choose <b>Website</b> and set the <b>Site URL</b> to <em>%s</em>'] = 'לחץ על <b>הוסף פלטפורמה</b>, בחר <b>אתר אינטרנט</b> וקבע את <b>כתובת האתר</b> ל-<em>%s</em>';
+$lang['Click on <b>Configure consent screen</b>, fill the <b>Product name</b> and save'] = 'לחץ על <b>הגדר מסך הסכמה</b>, מלא את <b>שם המוצר</b> ושמור';
+$lang['Enter <em>%1s</em> for <b>%2s</b>'] = 'הזן <em>%1s</em> עבור <b>%2s</b>';
+$lang['Fill out any required fields such as the application name and description'] = 'מלא את כל השדות הנדרשים כגון שם היישום והתיאור';
+$lang['Go to <a href="%s" target="_blank">%s</a> and create a new application'] = 'עבור ל<a href="%s" target="_blank">%s</a> וצור אפליקציה חדשה';
+$lang['Go to <b>API Parameters</b> and set <em>%s</em> for <b>Redirect URL</b>'] = 'עבור ל<b>פרמטרי API</b> וקבע <em>%s</em> עבור <b>כתובת ההפניה</b>';
+$lang['Go to <b>Settings -> Advanced</b> and set <b>Valid OAuth redirect URIs</b> to <em>%s</em>'] = 'עבור ל<b>הגדרות -> מתקדם</b> וקבע <b>כתובות הפניית OAuth תקפות</b> ל-<em>%s</em>';
+$lang['Go to <b>Settings -> Basic</b> and fill the contact email'] = 'עבור ל<b>הגדרות -> בסיסי</b> ומלא את כתובת האימייל ליצירת קשר';
+$lang['Go to <b>Status & Review</b> and set the app public by clicking the big button on top-right'] = 'עבור ל<b>מצב & ביקורת</b> והגדר את האפליקציה לציבורית על ידי לחיצה על הכפתור הגדול בצד ימין למעלה';
+$lang['Go to the <b>Library</b> tab, open <b>Social APIs -> Google+ API</b> and click <b>ENABLE</b>'] = 'עבור לכרטיסיית <b>ספרייה</b>, פתח <b>ממשקי API חברתיים -> API של Google+</b> ולחץ על <b>הפעל</b>';
+$lang['Go to the <b>Settings</b> tab after creating the app'] = 'עבור לכרטיסיית <b>הגדרות</b> לאחר יצירת האפליקציה';
+$lang['No registration required'] = 'אין צורך ברישום';
+$lang['On the <b>Credentials</b> tab, click <b>Create credentials -> OAuth client ID</b>'] = 'בכרטיסיית <b>אישורים</b>, לחץ על <b>צור אישורים -> מזהה לקוח OAuth</b>';
+$lang['Once the application is created, click <b>See application details</b>'] = 'לאחר יצירת האפליקציה, לחץ על <b>ראה פרטי אפליקציה</b>';
+$lang['Once the process is finished, click on the newly created project'] = 'לאחר הסיום של התהליך, לחץ על הפרויקט שנוצר באופן חדש';
+$lang['Once you have registered, copy and past the created application credentials into this setup page'] = 'לאחר הרישום, העתק והדבק את פרטי האפליקציה שנוצרו לתוך דף ההגדרות הזה';
+$lang['Put your website domain in the <b>%1s</b> field. It must match with the current hostname: <em>%2s</em>'] = 'הכנס את תחום האתר שלך בשדה <b>%1s</b>. זה חייב להתאים לשם המארח הנוכחי: <em>%2s</em>';
+$lang['Select these APIs: <b>Contacts</b> as <em>Read</em> and <b>Social Directory</b> as <em>Read Public</em>'] = 'בחר ב-APIs הבאים: <b>אנשי קשר</b> כ-<em>קריאה</em> ו-<b>מדריך חברתי</b> כ-<em>קריאה ציבורית</em>';
+$lang['Set <b>%1s</b> to <em>%2s</em>'] = 'הגדר את <b>%1s</b> ל-<em>%2s</em>';
---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,40 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['%s: invalid keys'] = '%s: מפתחות לא חוקיים';
+$lang['Allow users to merge existing account with new <i>Social Connect</i> identity'] = 'לאפשר למשתמשים למזג חשבון קיים עם זהות <i>Social Connect</i> חדשה';
+$lang['An error occured, please contact the gallery owner. <i>Error code : %s</i>'] = 'אירעה שגיאה, אנא צור קשר עם בעל הגלריה. <i>קוד שגיאה: %s</i>';
+$lang['Associate with an existing account'] = 'לקשר עם חשבון קיים';
+$lang['Authentication canceled'] = 'האימות בוטל';
+$lang['Cancel'] = 'ביטול';
+$lang['Close'] = 'סגירה';
+$lang['Contacting <b>%s</b>. Please wait.'] = 'מתקשר ל-<b>%s</b>. אנא המתן.';
+$lang['Disabled'] = 'מושבת';
+$lang['Display sign in buttons in the menubar'] = 'הצג לחצני כניסה בתפריט';
+$lang['Display sign in buttons on the register page'] = 'הצג לחצני כניסה בעמוד ההרשמה';
+$lang['Enabled'] = 'מופעל';
+$lang['Error...'] = 'שגיאה...';
+$lang['For security reason, the main webmaster account can\'t be merged with a remote account, but you can use another webmaster account.'] = 'מסיבות אבטחה, חשבון האדמין הראשי לא יכול להתמזג עם חשבון מרוחק, אך תוכל להשתמש בחשבון אחר.';
+$lang['Icon size in the menubar'] = 'גודל האייקונים בתפריט';
+$lang['Icon size on the identification page'] = 'גודל האייקונים בעמוד הזיהוי';
+$lang['Loading...'] = 'טוען...';
+$lang['Logged with'] = 'מחובר עם';
+$lang['Or sign in with'] = 'או התחבר עם';
+$lang['Please enter your OpenID URL'] = 'אנא הזן את כתובת ה-OpenID שלך';
+$lang['Please enter your user ID'] = 'אנא הזן את מזהה המשתמש שלך';
+$lang['Please enter your username'] = 'אנא הזן את שם המשתמש שלך';
+$lang['Profile URL'] = 'כתובת URL של הפרופיל';
+$lang['Providers'] = 'ספקים';
+$lang['Sign in with %s'] = 'התחבר עם %s';
+$lang['Social Connect: PHP Curl extension is needed'] = 'Social Connect: נדרשת תוספת PHP Curl';
+$lang['Social Connect: You need to configure the credentials'] = 'Social Connect: עליך להגדיר את האישורים';
+$lang['Sorry, new registrations are blocked on this gallery.'] = 'מצטערים, ההרשמות החדשות נחסמות בגלריה זו.';
+$lang['Unable to perform login'] = 'לא ניתן לבצע התחברות';
+$lang['User not found'] = 'משתמש לא נמצא';
+$lang['Users are not allowed to register on your gallery. Social Connect will not work correctly.'] = 'המשתמשים אינם מורשים להירשם לגלריה שלך. Social Connect לא יעבוד כראוי.';
+$lang['You registered with a %s account, please sign in with the same account.'] = 'נרשמת עם חשבון %s, אנא התחבר עם אותו חשבון.';
+$lang['Your registration is almost done, please complete the registration form.'] = 'ההרשמה שלך כמעט הושלמה, אנא השלם את טופס ההרשמה.';
