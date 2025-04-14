a4f9f336f1fadd508d89ce11bd392fb96e457e48
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-04-14 20:30:33 +0200

    Update fi_FI, thanks to Jusaus

---
+++ language/fi_FI/help.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Based on OpenID'] = 'Perustuu OpenID:hen';
+$lang['Click on <b>Add Platform</b>, choose <b>Website</b> and set the <b>Site URL</b> to <em>%s</em>'] = 'Klikkaa <b>Lisää alusta</b>, valitse <b>Verkkosivusto</b> ja aseta <b>Sivuston URL-osoitteeksi</b> <em>%s</em>';
+$lang['Click on <b>Configure consent screen</b>, fill the <b>Product name</b> and save'] = 'Klikkaa <b>Määritä suostumusnäyttö</b>, täytä <b>Tuotteen nimi</b> ja tallenna';
+$lang['Enter <em>%1s</em> for <b>%2s</b>'] = 'Anna <em>%1s</em> kenttään <b>%2s</b>';
+$lang['Fill out any required fields such as the application name and description'] = 'Täytä kaikki vaaditut kentät, kuten sovelluksen nimi ja kuvaus';
+$lang['Go to <a href="%s" target="_blank">%s</a> and create a new application'] = 'Siirry osoitteeseen <a href="%s" target="_blank">%s</a> ja luo uusi sovellus';
+$lang['Go to <b>API Parameters</b> and set <em>%s</em> for <b>Redirect URL</b>'] = 'Siirry kohtaan <b>API-parametrit</b> ja aseta <em>%s</em> kenttään <b>Uudelleenohjauksen URL</b>';
+$lang['Go to <b>Settings -> Advanced</b> and set <b>Valid OAuth redirect URIs</b> to <em>%s</em>'] = 'Siirry kohtaan <b>Asetukset -> Lisäasetukset</b> ja aseta <b>Validit OAuth-uudelleenohjaus-URI:t</b> arvoksi <em>%s</em>';
+$lang['Go to <b>Settings -> Basic</b> and fill the contact email'] = 'Siirry kohtaan <b>Asetukset -> Perusasetukset</b> ja täytä yhteyssähköpostiosoite';
+$lang['Go to <b>Status & Review</b> and set the app public by clicking the big button on top-right'] = 'Siirry kohtaan <b>Tila ja tarkistus</b> ja aseta sovellus julkiseksi klikkaamalla oikean yläkulman isoa painiketta';
+$lang['Go to the <b>Library</b> tab, open <b>Social APIs -> Google+ API</b> and click <b>ENABLE</b>'] = 'Siirry <b>Kirjasto</b>-välilehdelle, avaa <b>Sosiaaliset API:t -> Google+ API</b> ja klikkaa <b>OTA KÄYTTÖÖN</b>';
+$lang['Go to the <b>Settings</b> tab after creating the app'] = 'Siirry <b>Asetukset</b>-välilehdelle sovelluksen luonnin jälkeen';
+$lang['No registration required'] = 'Rekisteröityimistä ei vaadita';
+$lang['On the <b>Credentials</b> tab, click <b>Create credentials -> OAuth client ID</b>'] = '<b>Tunnistetiedot</b>-välilehdellä klikkaa <b>Luo tunnistetiedot -> OAuth-asiakastunnus</b>';
+$lang['Once the application is created, click <b>See application details</b>'] = 'Kun sovellus on luotu, klikkaa <b>Katso sovelluksen tiedot</b>';
+$lang['Once the process is finished, click on the newly created project'] = 'Kun prosessi on valmis, klikkaa vastaluotua projektia';
+$lang['Once you have registered, copy and past the created application credentials into this setup page'] = 'Kun olet rekisteröitynyt, kopioi ja liitä luodut sovellustunnukset tälle asetussivulle';
+$lang['Put your website domain in the <b>%1s</b> field. It must match with the current hostname: <em>%2s</em>'] = 'Syötä verkkosivustosi domain kenttään <b>%1s</b>. Sen on vastattava nykyistä isäntänimeä: <em>%2s</em>';
+$lang['Select these APIs: <b>Contacts</b> as <em>Read</em> and <b>Social Directory</b> as <em>Read Public</em>'] = 'Valitse seuraavat API:t: <b>Contacts</b> asetuksella <em>Read</em> ja <b>Social Directory</b> asetuksella <em>Read Public</em>';
+$lang['Set <b>%1s</b> to <em>%2s</em>'] = 'Aseta <b>%1s</b> arvoksi <em>%2s</em>';
---
+++ language/fi_FI/plugin.lang.php
@@ -0,0 +1,40 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Please enter your user ID'] = 'Anna käyttäjätunnuksesi (ID)';
+$lang['Please enter your username'] = 'Anna käyttäjänimesi';
+$lang['Profile URL'] = 'Profiilin URL-osoite';
+$lang['Providers'] = 'Palveluntarjoajat';
+$lang['Sign in with %s'] = 'Kirjaudu sisään käyttäen %s';
+$lang['Social Connect: PHP Curl extension is needed'] = 'Social Connect: PHP Curl -laajennus vaaditaan';
+$lang['Social Connect: You need to configure the credentials'] = 'Social Connect: Sinun täytyy määrittää tunnistetiedot';
+$lang['Sorry, new registrations are blocked on this gallery.'] = 'Valitettavasti uudet rekisteröitymiset on estetty tässä galleriassa.';
+$lang['Unable to perform login'] = 'Kirjautuminen epäonnistui';
+$lang['User not found'] = 'Käyttäjää ei löydy';
+$lang['Users are not allowed to register on your gallery. Social Connect will not work correctly.'] = 'Käyttäjien rekisteröityminen galleriaasi ei ole sallittua. Social Connect ei toimi oikein.';
+$lang['You registered with a %s account, please sign in with the same account.'] = 'Rekisteröidyit %s-tilillä, ole hyvä ja kirjaudu sisään samalla tilillä.';
+$lang['Your registration is almost done, please complete the registration form.'] = 'Rekisteröitymisesi on melkein valmis, ole hyvä ja täytä rekisteröitymislomake loppuun.';
+$lang['%s: invalid keys'] = '%s: virheelliset avaimet';
+$lang['Allow users to merge existing account with new <i>Social Connect</i> identity'] = 'Salli käyttäjien yhdistää olemassa oleva tili uuteen <i>Social Connect</i> -tunnukseen';
+$lang['An error occured, please contact the gallery owner. <i>Error code : %s</i>'] = 'Tapahtui virhe, ole hyvä ja ota yhteyttä gallerian ylläpitäjään. <i>Virhekoodi: %s</i>';
+$lang['Associate with an existing account'] = 'Yhdistä olemassa olevaan tiliin';
+$lang['Authentication canceled'] = 'Tunnistautuminen peruutettu';
+$lang['Cancel'] = 'Peruuta';
+$lang['Close'] = 'Sulje';
+$lang['Contacting <b>%s</b>. Please wait.'] = 'Yhdistetään kohteeseen <b>%s</b>. Odota hetki.';
+$lang['Disabled'] = 'Poistettu käytöstä';
+$lang['Display sign in buttons in the menubar'] = 'Näytä kirjautumispainikkeet valikkopalkissa';
+$lang['Display sign in buttons on the register page'] = 'Näytä kirjautumispainikkeet rekisteröitymissivulla';
+$lang['Enabled'] = 'Käytössä';
+$lang['Error...'] = 'Virhe...';
+$lang['For security reason, the main webmaster account can\'t be merged with a remote account, but you can use another webmaster account.'] = 'Turvallisuussyistä pääwebmasterin tiliä ei voi yhdistää etätiliin, mutta voit käyttää toista webmaster-tiliä.';
+$lang['Icon size in the menubar'] = 'Kuvakkeen koko valikkopalkissa';
+$lang['Icon size on the identification page'] = 'Kuvakkeen koko tunnistautumissivulla';
+$lang['Loading...'] = 'Ladataan...';
+$lang['Logged with'] = 'Kirjautuneena käyttäjällä';
+$lang['Or sign in with'] = 'Tai kirjaudu sisään käyttäen';
+$lang['Please enter your OpenID URL'] = 'Anna OpenID-osoitteesi';
